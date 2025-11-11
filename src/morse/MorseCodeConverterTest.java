c5cffc24d1b4a533d04ba523d077b8892bc368cf
Allen493 <allen.albadri@hotmail.com>
2025-11-11 05:07:26 +0100

    Update test case for Morse to text conversion

--- src/morse/MorseCodeConverterTest.java
+++ src/morse/MorseCodeConverterTest.java
@@ -13,7 +13,7 @@ public class MorseCodeConverterTest {
     @Test
     public void testMorseToText() {
         MorseCodeConverter converter = new MorseCodeConverter();
-        assertEquals("Bajs", converter.morseToEnglish(".... . .-.. .-.. ---"));
+        assertEquals("HELLO", converter.morseToEnglish(".... . .-.. .-.. ---"));
     }
 
     @Test
