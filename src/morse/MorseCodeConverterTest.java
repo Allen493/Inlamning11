c695d46ff0a8214515b1468ed75ed3b1ca171e13
Allen <allen.albadri@hotmail.com>
2025-11-11 04:46:47 +0100

    commit

--- src/morse/MorseCodeConverterTest.java
+++ src/morse/MorseCodeConverterTest.java
@@ -13,7 +13,7 @@ public class MorseCodeConverterTest {
     @Test
     public void testMorseToText() {
         MorseCodeConverter converter = new MorseCodeConverter();
-        assertEquals("HELLO", converter.morseToEnglish(".... . .-.. .-.. ---"));
+        assertEquals("HELgggggggggggggggggrLO", converter.morseToEnglish(".... . .-.. .-.. ---"));
     }
 
     @Test
